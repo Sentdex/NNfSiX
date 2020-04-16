5a3d231fc7b2c331ab87713da2555f5c61f404a7
Harrison <harrison@pythonprogramming.net>
2020-04-16 14:41:24 -0500

    Update p1-Basic-Neuron-3-inputs.py

--- Python/p1-Basic-Neuron-3-inputs.py
+++ Python/p1-Basic-Neuron-3-inputs.py
@@ -1,6 +1,7 @@
-# Associated YT NNFS tutorial: https://www.youtube.com/watch?v=Wo5dMEP_BbI
 '''
 Creates a basic neuron with 3 inputs.
+
+Associated YT NNFS tutorial: https://www.youtube.com/watch?v=Wo5dMEP_BbI
 '''
 
 inputs = [1.2, 5.1, 2.1]
