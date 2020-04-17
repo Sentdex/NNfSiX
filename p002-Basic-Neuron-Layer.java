d79a9e83032fd8d0c374395f233a89590a9eff72
Tyler J. Kozlowski <Tyler.J.Kozlowski@gmail.com>
2020-04-17 17:49:17 -0500

    fixed formatting

--- p002-Basic-Neuron-Layer.java
+++ p002-Basic-Neuron-Layer.java
@@ -17,8 +17,8 @@ class Neuron {
 		double bias3 = 0.5;
 
 		double[] output = {(inputs[0] * weights1[0] + inputs[1] * weights1[1] + inputs[2] * weights1[2] + bias1), 
-						   (inputs[0] * weights2[0] + inputs[1] * weights2[1] + inputs[2] * weights2[2] + bias2), 
-						   (inputs[0] * weights3[0] + inputs[1] * weights3[1] + inputs[2] * weights3[2] + bias3)};
+			           (inputs[0] * weights2[0] + inputs[1] * weights2[1] + inputs[2] * weights2[2] + bias2), 
+			           (inputs[0] * weights3[0] + inputs[1] * weights3[1] + inputs[2] * weights3[2] + bias3)};
 		System.out.println(output);
 	}
 }
