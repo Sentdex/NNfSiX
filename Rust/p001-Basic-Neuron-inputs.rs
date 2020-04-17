eae8b196f94307e9492eded153401dbe1d4df48b
Tone King <king@Tones-MBP.home>
2020-04-17 04:56:10 -0400

    Adding Rust Neuron

---
+++ Rust/p001-Basic-Neuron-inputs.rs
@@ -0,0 +1,15 @@
+
+// Creates a basic neuron with 3 inputs.
+
+// Associated YT NNFS tutorial: https://www.youtube.com/watch?v=Wo5dMEP_BbI
+
+fn main() {
+    let inputs = vec![1.2, 5.1, 2.1];
+    let weights = vec![3.1, 2.1, 8.7];
+    let bias = 3.0;
+    let output = inputs[0] * weights[0] +
+                 inputs[1] * weights[1] +
+                 inputs[2] * weights[2] +
+                 bias;
+    println!("{}", output);
+}
