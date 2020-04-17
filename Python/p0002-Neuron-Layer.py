03628e5506d1238552ab89b5a9f3a7e29bbd387b
Harrison <harrison@pythonprogramming.net>
2020-04-17 08:59:32 -0500

    Create p0002-Neuron-Layer.py

---
+++ Python/p0002-Neuron-Layer.py
@@ -0,0 +1,19 @@
+'''
+Creates a simple layer of neurons, with 4 inputs.
+Associated YT NNFS tutorial: https://www.youtube.com/watch?v=lGLto9Xd7bU
+'''
+
+inputs = [1.0, 2.0, 3.0, 2.5]
+
+weights1 = [0.2, 0.8, -0.5, 1.0]
+weights2 = [0.5, -0.91, 0.26, -0.5]
+weights3 = [-0.26, -0.27, 0.17, 0.87]
+
+bias1 = 2.0
+bias2 = 3.0
+bias3 = 0.5
+
+output = [inputs[0]*weights1[0] + inputs[1]*weights1[1] + inputs[2]*weights1[2] + inputs[3]*weights1[3] + bias1,
+          inputs[0]*weights2[0] + inputs[1]*weights2[1] + inputs[2]*weights2[2] + inputs[3]*weights2[3] + bias2,
+          inputs[0]*weights3[0] + inputs[1]*weights3[1] + inputs[2]*weights3[2] + inputs[3]*weights3[3] + bias3]
+print(output)
