b357fb5e29958ee5d863bf5be314a550160843b3
Daniel Kukieła <daniel@kukiela.pl>
2020-04-17 16:36:59 +0200

    Reverting changes from pull request

--- C++/p001-Basic-Neuron-3-inputs.cpp
+++ C++/p001-Basic-Neuron-3-inputs.cpp
@@ -5,39 +5,16 @@
  */
 
 #include <iostream>
-#include <vector>
 
-using namespace std;
-
-double neuron(vector<double> i, vector<double> w, double bias) 
-{
-    double output = 0;
-
-    for(int a = 0; a < i.size() && a < w.size(); a++)
-    {
-        output += i[a] * w[a];
-    }
-    output += bias;
-
-    return output;
-}
-
-int main() 
-{
-    vector<double> inputs;
-    vector<double> weights;
-
-    inputs.push_back(1.2);
-    inputs.push_back(5.1);
-    inputs.push_back(2.1);
-
-    weights.push_back(3.1);
-    weights.push_back(2.1);
-    weights.push_back(8.7);
-
+int main() {
+    double inputs[] = {1.2, 5.1, 2.1};
+    double weights[] = {3.1, 2.1, 8.7};
     double bias = 3.0;
-    
-    double output = neuron(inputs, weights, bias);
 
-    cout << "Output : " << output << endl;;
+    double output = inputs[0] * weights[0] +
+                    inputs[1] * weights[1] + 
+                    inputs[2] * weights[2] + 
+                    bias;
+
+    std::cout << output << std::endl;
 }
