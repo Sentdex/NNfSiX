5e0e376bbfd200cc7dc81837916525eefa68a06c
rish-16 <mail.rishabh.anand@gmail.com>
2020-04-17 14:00:18 +0800

    Added Java Neuron

---
+++ Java/p001-Basic-Neuron-3-inputs.java
@@ -0,0 +1,14 @@
+/*
+Create a basic neuron with 3 inputs in Java
+*/
+
+class Neuron {
+	public static void main(String[] args) {
+		double[] inputs = {1.2, 5.1, 2.1};
+		double[] weights = {3.1, 2.1, 8.7};
+		double bias = 3.1;
+
+		double output = inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2] + bias;
+		System.out.println(output);
+	}
+}
