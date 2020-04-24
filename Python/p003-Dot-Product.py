4f3c8917b1f94f5c5ffd3fedc4e290b8e7a1a332
Harrison <Harrison@sentdex.com>
2020-04-24 09:49:16 -0500

    Create p003-Dot-Product.py

---
+++ Python/p003-Dot-Product.py
@@ -0,0 +1,11 @@
+import numpy as np 
+
+inputs = [1.0, 2.0, 3.0, 2.5]
+weights = [[0.2, 0.8, -0.5, 1.0],
+           [0.5, -0.91, 0.26, -0.5],
+           [-0.26, -0.27, 0.17, 0.87]]
+
+biases = [2.0, 3.0, 0.5]
+
+output = np.dot(weights, inputs) + biases
+print(output)
