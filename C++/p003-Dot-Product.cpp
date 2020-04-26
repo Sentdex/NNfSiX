83edbc95b2430123558392b5458a4abbc3ded107
Shreeviknesh <35604420+shreeviknesh@users.noreply.github.com>
2020-04-26 19:34:37 +0530

    Update p003-Dot-Product.cpp - Fixed Indentation
    The code was hard to read with the mixed indentation of spaces and tabs. Changed it to maintain consistency.

--- C++/p003-Dot-Product.cpp
+++ C++/p003-Dot-Product.cpp
@@ -39,10 +39,10 @@ int main() {
 	vector<double> inputs = {1.0, 2.0, 3.0, 2.5};
 
 	vector<vector<double>> weights = {{0.2, 0.8, -0.5, 1.0},
-									  {0.5, -0.91, 0.26, -0.5},
-									  {-0.26, -0.27, 0.17, 0.87}};
-
-    vector<double> biases = {2.0, 3.0, 0.5};
+					  {0.5, -0.91, 0.26, -0.5},
+					  {-0.26, -0.27, 0.17, 0.87}};
+	
+	vector<double> biases = {2.0, 3.0, 0.5};
 
 	vector<double> output = add(dotProduct(weights, inputs), biases);
 
