6bd008196a7a4ad59d9e9e18c069a810bad902bc
clubPenguin420 <42702641+clubPenguin420@users.noreply.github.com>
2020-04-27 09:35:59 -0500

    Added a main function

--- Kotlin/P001BasicNeuron3Inputs.kt
+++ Kotlin/P001BasicNeuron3Inputs.kt
@@ -3,9 +3,11 @@
  * Associated YT NNFS tutorial: https://www.youtube.com/watch?v=Wo5dMEP_BbI
 */
 
-val inputs = listOf(1.2, 5.1, 2.1)
-val weights = listOf(3.1, 2.1, 8.7)
-val bias = 3
+fun main(){
+ val inputs = listOf(1.2, 5.1, 2.1)
+ val weights = listOf(3.1, 2.1, 8.7)
+ val bias = 3
 
-val output = inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2] + bias
-println(output)
+ val output = inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2] + bias
+ println(output)
+}
