c7f2f87129dfa6772509f2c798f4e9d24de986f5
markryanandrews <markryanandrews794@gmail.com>
2020-04-27 02:04:59 -0400

    P3 Dot Product in R

---
+++ R/p003.r
@@ -0,0 +1,15 @@
+if(! "pracma" %in% installed.packages()){
+  install.packages("pracma", dependencies = TRUE)
+}
+require("pracma")
+
+inputs <- c(1, 2, 3, 2.5)
+
+weights <- list( c(0.2, 0.8, -0.5, 1.0), 
+                 c(0.5, -0.91, 0.26, -0.5), 
+                 c(-0.26, -0.27, 0.17, 0.87))
+
+biases <- c(2,3,0.5)
+
+output <- dot(do.call(cbind, weights), inputs) + biases
+print(output)
