519e06883aec09fd23fae9b0cb4786923694058b
Roberto Tomás Collins <roberto.tomas.cuentas@gmail.com>
2020-05-01 08:59:56 -0400

    revised to extensions

--- Swift/p003-Dot-Product.swift
+++ Swift/p003-Dot-Product.swift
@@ -1,34 +1,47 @@
-extension Array where Element == [Double] {
-    func dot (_ rval: [Double]) -> [Double] {
-        // here one should check for lvals[n].count == rval.count
-        // but this will be left out for simplicity and readability
-        return self.map { (lval) -> Double in lval.dot(rval) }
-    }
-}
-
+/*
+Doing dot product with a layer of neurons and multiple inputs
+Associated YT NNFS tutorial: https://www.youtube.com/watch?v=tMrbN67U9d4
+*/
 extension Array where Element == Double {
-    func dot (_ rval: [Double]) -> Double {
-        // here one should check for lval.count == rval.count
-        // but this will be left out for simplicity and readability
-        return zip(self, rval)
-            .reduce(0.0, { (sum, tuple) -> Double in sum + tuple.0 * tuple.1 })
+    func dot (_ rval: [Double]) -> Double? {
+        if self.count != rval.count { return nil }
+        return zip(self, rval).reduce(0.0, { (sum, tuple) -> Double in
+            sum + tuple.0 * tuple.1
+        })
     }
 
-    func add(_ rval: [Double]) -> [Double] {
-        // here one should check for self.count == rval.count
-        // but this will be left out for simplicity and readability
+    func add(_ rval: [Double]) -> [Double]? {
+        if self.count != rval.count { return nil }
         return zip(self, rval).map(+)
     }
 }
 
-// The above replaces the `import numpy as np` =========
+extension Array where Element == [Double] {
+    func dot (_ rval: [Double]) -> [Double]? {
+        let rc = rval.count
+        if self.reduce(false, { $1.count != rc }) { return nil }
 
-var inputs:[Double] = [1.0, 2.0, 3.0, 2.5]
-var weights:[[Double]] = [[0.2, 0.8, -0.5, 1.0],
+        var resultArray: [Double] = []
+        for lval in self{
+            guard let dotProduct = lval.dot(rval) else {
+                return nil
+            }
+            resultArray.append(dotProduct)
+        }
+        
+        return resultArray
+    }
+}
+// The above replaces numpy =========
+
+let inputs:[Double] = [1.0, 2.0, 3.0, 2.5]
+let weights:[[Double]] = [[0.2, 0.8, -0.5, 1.0],
                           [0.5, -0.91, 0.26, -0.5],
                           [-0.26, -0.27, 0.17, 0.87]]
 
-var biases:[Double] = [2.0, 3.0, 0.5]
+let biases:[Double] = [2.0, 3.0, 0.5]
 
-var output:[Double] = weights.dot(inputs).add(biases)
+guard let output:[Double] = weights.dot(inputs)?.add(biases) else {
+    fatalError("Unmatched shapes")
+}
 print(output)
