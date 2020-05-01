95862069d01da3a07247bbb2d5d4dc7aaefdb2e2
Roberto Tomás Collins <roberto.tomas.cuentas@gmail.com>
2020-04-30 21:08:53 -0400

    propagated the previous change through the rest of the code

--- Swift/p003-Dot-Product.swift
+++ Swift/p003-Dot-Product.swift
@@ -1,17 +1,19 @@
-func dot (_ lvals: [[Double]], _ rval: [Double]) -> [Double] {
-    // here one should check for lvals[n].count == rval.count
-    // but this will be left out for simplicity and readability
-    return lvals.map { (lval) -> Double in dot(lval,rval) }
-}
-
-func dot (_ lval: [Double], _ rval: [Double]) -> Double {
-    // here one should check for lval.count == rval.count
-    // but this will be left out for simplicity and readability
-    return zip(lval, rval)
-        .reduce(0.0, { (sum, tuple) -> Double in sum + tuple.0 * tuple.1 })
+extension Array where Element == [Double] {
+    func dot (_ rval: [Double]) -> [Double] {
+        // here one should check for lvals[n].count == rval.count
+        // but this will be left out for simplicity and readability
+        return self.map { (lval) -> Double in lval.dot(rval) }
+    }
 }
 
 extension Array where Element == Double {
+    func dot (_ rval: [Double]) -> Double {
+        // here one should check for lval.count == rval.count
+        // but this will be left out for simplicity and readability
+        return zip(self, rval)
+            .reduce(0.0, { (sum, tuple) -> Double in sum + tuple.0 * tuple.1 })
+    }
+
     // here one should check for self.count == rval.count
     // but this will be left out for simplicity and readability
     func add(_ rval: [Double]) -> [Double] {
@@ -28,5 +30,5 @@ var weights:[[Double]] = [[0.2, 0.8, -0.5, 1.0],
 
 var biases:[Double] = [2.0, 3.0, 0.5]
 
-var output:[Double] = dot(weights, inputs).add(biases)
+var output:[Double] = weights.dot(inputs).add(biases)
 print(output)
