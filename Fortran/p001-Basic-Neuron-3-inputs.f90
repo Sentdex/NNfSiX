78f122918013237ae18e1bf96dcaf605551d7bb7
hunar1997 <hbkurd@gmail.com>
2020-05-02 13:18:50 +0300

    Added the fourth Fortran code

--- Fortran/p001-Basic-Neuron-3-inputs.f90
+++ Fortran/p001-Basic-Neuron-3-inputs.f90
@@ -4,7 +4,7 @@ program NN
 
     inputs  = [1.2, 5.1, 2.1]
     weights = [3.1, 2.1, 8.7]
-    bias	= 3
+    bias    = 3
 
     output = inputs(1) * weights(1) +&
              inputs(2) * weights(2) +&
