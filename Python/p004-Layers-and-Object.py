2ac342bbfca26e6e51217943910a2bbc9667560d
Gourav Kolhatkar <gstar1525@gmail.com>
2020-05-03 23:19:59 -0700

    Update p004-Layers-and-Object.py

--- Python/p004-Layers-and-Object.py
+++ Python/p004-Layers-and-Object.py
@@ -18,10 +18,10 @@ class Layer_Dense:
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
 
-layer1 = Layer_Dense(4,5)
-layer2 = Layer_Dense(5,2)
-
+layer1 = Layer_Dense(np.array(X).shape[1], 5)
 layer1.forward(X)
-#print(layer1.output)
+#print(layer1.output) 
+
+layer2 = Layer_Dense(np.array(layer1.output).shape[1], 2)
 layer2.forward(layer1.output)
 print(layer2.output)
