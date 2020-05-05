a6ec2fd7ceb8d4eca2edcf5ddc5c5a1fdc62fbbd
SCOTT-HAMILTON <sgn.hamilton@protonmail.com>
2020-05-05 13:09:48 +0200

    C++ own implementation

--- C++/p003-Dot-Product.cpp
+++ C++/p003-Dot-Product.cpp
@@ -3,51 +3,53 @@
  * 
  * Associated tutorial https://www.youtube.com/watch?v=tMrbN67U9d4
  */
-
+#include <algorithm>
 #include <iostream>
+#include <iterator>
+#include <numeric>
+#include <tuple>
 #include <vector>
-using std::vector;
 
-
-vector<double> dotProduct(vector<vector<double>> weights, vector<double> inputs) {
-	vector<double> outputs(weights.size());
-
-	for(int i = 0; i < weights.size(); i++) {
-		double output = 0;
-		for(int j = 0; j < inputs.size(); j++) {
-			output += weights[i][j] * inputs[j];
-		}
-		outputs[i] = output;
+// Simple implementation of built-in python zip
+template<typename T, typename P>
+std::vector<std::tuple<T, P>> zip(typename std::vector<T>::iterator firstArrayBegin, 
+			 typename std::vector<P>::iterator secondArrayBegin,
+			 typename std::vector<P>::const_iterator secondArrayEnd)
+{
+	const auto size = std::distance(static_cast<decltype(secondArrayEnd)>(secondArrayBegin), secondArrayEnd);
+	std::vector<std::tuple<T, P>> zipped;
+	zipped.reserve(size);
+	while (secondArrayBegin != secondArrayEnd) {
+		zipped.emplace_back(*firstArrayBegin++, *secondArrayBegin++);
 	}
-
-	return outputs;
+	return zipped;
 }
-
-vector<double> add(vector<double> vector1, vector<double> vector2) {
-	vector<double> output(vector1.size());
-
-	for(int i = 0; i < vector1.size(); i++) {
-		output[i] = vector1[i] + vector2[i];
-	}
-
-	return output;
-}
-
 
 int main() {
+	// Inputs
+	std::vector<double> inputs{1.0, 2.0, 3.0, 2.5};
+	std::vector<std::vector<double>> weights{{.2, 0.8, -0.5, 1.0},
+		{0.5, -0.91, 0.26, -0.5},
+		{-0.26, -0.27, 0.17, 0.87}};
+	std::vector<double> biases{2.0, 3.0, 0.5};
+	
+	// Zipping
+	auto zipped_weights_and_biases = 
+		zip<decltype(weights)::value_type, double>(weights.begin(), biases.begin(), biases.end());
+	
+	// Calculating
+	std::vector<double> outputs;
+	outputs.reserve(std::size(zipped_weights_and_biases));
+	std::transform(std::begin(zipped_weights_and_biases), std::end(zipped_weights_and_biases), 
+		    std::back_inserter(outputs),  [&inputs](const auto &weightAndBias) {
+	     return std::inner_product(std::begin(std::get<0>(weightAndBias)), 
+			     std::end(std::get<0>(weightAndBias)), 
+			     std::begin(inputs), 0.0)+std::get<1>(weightAndBias); 
+	}); 
 
-	vector<double> inputs = {1.0, 2.0, 3.0, 2.5};
-
-	vector<vector<double>> weights = {{0.2, 0.8, -0.5, 1.0},
-									  {0.5, -0.91, 0.26, -0.5},
-									  {-0.26, -0.27, 0.17, 0.87}};
-
-    vector<double> biases = {2.0, 3.0, 0.5};
-
-	vector<double> output = add(dotProduct(weights, inputs), biases);
-
-	std::cout << "[";
-	std::cout << output[0] << ", ";
-	std::cout << output[1] << ", ";
-	std::cout << output[2] << "]" << std::endl;
+	// Ouputing
+	std::cout << '[';
+	std::copy(outputs.begin(), outputs.end(), std::ostream_iterator<double>(std::cout, " "));
+	std::cout << ']';
+	return 0;
 }
--- C++/p004-Layers-and-Object.cpp
+++ C++/p004-Layers-and-Object.cpp
@@ -3,111 +3,205 @@
  * 
  * Associated tutorial https://www.youtube.com/watch?v=TEWy9vZcxW4
  */
+#include <algorithm>
+#include <array>
+#include <chrono>
+#include <initializer_list>
 #include <iostream>
+#include <iterator>
 #include <random>
-#include <vector>
-using namespace std;
+#include <type_traits>
 
-/* A helper function to get a random float in range [low, high] */
-double getRandomDouble(double low, double high) {
-	random_device rd;  
-    mt19937 gen(rd()); 
-    uniform_real_distribution<> dis(low, high);
-    return dis(gen);
+/// Types
+template<std::size_t size>
+struct SizeType 
+{
+	static constexpr std::size_t get(){ return size;}
+};
+
+template<class T, std::size_t s>
+struct List
+{
+	static constexpr SizeType<s> width;
+	static constexpr SizeType<1> height;
+	static constexpr SizeType<s> size;
+	std::array<T, s> list;
+	constexpr inline auto begin() noexcept { return list.begin();};
+	constexpr inline auto end() noexcept { return list.end();};
+	constexpr inline auto begin() const noexcept { return list.cbegin();};
+	constexpr inline auto end() const noexcept { return list.cend();};
+	typedef T value_type;
+};
+
+template<std::size_t w, std::size_t h>
+struct Matrix
+{
+	static constexpr SizeType<w> width;
+	static constexpr SizeType<h> height;
+	static constexpr SizeType<h> size;
+	List<List<double, w>, h> matrix;
+	constexpr inline auto begin() noexcept { return matrix.begin();};
+	constexpr inline auto end() noexcept { return matrix.end();};
+	constexpr inline auto begin() const noexcept { return matrix.begin();};
+	constexpr inline auto end() const noexcept { return matrix.end();};
+	typedef typename decltype(matrix)::value_type value_type;
+};
+
+template <class L> 
+auto pretty_print(const L& list) -> 
+	decltype(std::declval<L>().list, void()) { 
+	std::cout << "[ ";
+	std::copy(std::begin(list), std::end(list), std::ostream_iterator<double>(std::cout, " "));
+	std::cout << "]\n";
+} 
+
+
+template<class M>
+auto pretty_print(const M& matrix) -> 
+	decltype(std::declval<M>().matrix, void()) { 
+	std::for_each(std::begin(matrix), std::end(matrix),
+			[](const auto& list) {
+			pretty_print(list);
+		}
+	);
+} 
+// python zip built-in
+template<class M, class N>
+auto zip(const M& m, const N& n)
+{
+	constexpr auto m_s = decltype(m.size)::get();
+	constexpr auto n_s = decltype(n.size)::get();
+        constexpr auto size = std::min(m_s, n_s);
+        List<std::tuple<typename M::value_type, typename N::value_type>, size> zipped;
+	for (auto [it, mIt, nIt] = std::tuple{std::begin(zipped), std::begin(m), std::begin(n)};
+			(mIt != std::end(m) && nIt != std::end(n));
+		++mIt, ++nIt, ++it) {
+		*it = {*mIt, *nIt};
+	}
+        return zipped;
 }
 
-/* A dot product function */
-vector<vector<double>> dotProduct(vector<vector<double>> inputs, vector<vector<double>> weights) {
-	// If the inputs is of size (a, b) and weights is of size (b, c) then outputs is of size (a, c)
-	vector<vector<double>> outputs = vector<vector<double>>(inputs.size(), vector<double>(weights[0].size()));
+// numpy randn
+template <class L>
+auto randn(L& list, double factor) ->
+		decltype(std::declval<L>().list, void()) 
+{
+	std::mt19937 generator ( /* Arbitrary Seed */ 0);
+	std::generate(std::begin(list), std::end(list), 
+	[&](){
+		return static_cast<double>(generator())/
+		static_cast<double>(generator.max())*factor;
+	});
+}
 
-	// Calculating the dot product
-	for(int i = 0; i < inputs.size(); i++) {
-		for(int j = 0; j < weights[0].size(); j++) {
-			double output = 0;
-			for(int k = 0; k < inputs[0].size(); k++) {
-				output += inputs[i][k] * weights[k][j];
-			}
-			outputs[i][j] = output;
-		}
-	}
+// numpy randn
+template <class M>
+auto randn(M& matrix, double factor) ->
+		decltype(std::declval<M>().matrix, void()) 
+{
+	std::for_each(std::begin(matrix), std::end(matrix), [&](auto& list) {
+		randn(list, factor);
+	});
+}
 
+// numpy zeros
+template <class L>
+auto zeros(L& list) ->
+	decltype(std::declval<L>().list, void()) 
+{
+	std::fill(std::begin(list), std::end(list), 0);
+}
+
+template <class M>
+auto zeros(M& matrix) ->
+	decltype(std::declval<M>().matrix, void()) 
+{
+	std::for_each(std::begin(matrix), std::end(matrix), [](auto& list){
+		std::fill(std::begin(list), std::end(list), 0);
+	});
+}
+
+// numpy dot product
+template <class M, class N, class O>
+auto dot(M& inputs, N& weights, O& biases)
+{
+	constexpr auto width = decltype(weights.height)::get();
+	constexpr auto height = decltype(inputs.height)::get();
+
+	constexpr auto inputs_w = decltype(inputs.width)::get();
+	constexpr auto inputs_h = decltype(inputs.height)::get();
+	constexpr auto weights_w = decltype(weights.width)::get();
+	constexpr auto weights_h = decltype(weights.height)::get();
+	
+	constexpr auto biases_w = decltype(biases.width)::get();
+
+	static_assert(inputs_w == weights_w, "Inputs.w != Weights.w");
+	static_assert(biases_w == weights_h, "Biases.w != Weights.h");
+	Matrix<width, height> outputs;
+	zeros(outputs);
+
+	auto zipped_weights_and_biases = zip(weights, biases);
+	std::transform(std::begin(inputs), std::end(inputs), std::begin(outputs),
+			[width, &zipped_weights_and_biases](const auto& input){
+		List<double, width> outputLine;
+		std::transform(std::begin(zipped_weights_and_biases), std::end(zipped_weights_and_biases), std::begin(outputLine),
+				[&input, &zipped_weights_and_biases](const auto& zipped_weights_and_biasesLine){
+			/* std::cout << "Weights Line "; pretty_print(weightsLine); */
+			/* std::cout << "Input : ";pretty_print(input); */
+			double result = std::inner_product(std::begin(input), std::end(input), std::begin(std::get<0>(zipped_weights_and_biasesLine)), 0.0f)+std::get<1>(zipped_weights_and_biasesLine);
+			return result;
+		});
+		return outputLine;
+	});
 	return outputs;
 }
 
-/* A function to add two vectors */
-vector<double> add(vector<double> vector1, vector<double> vector2) {
-	vector<double> output(vector1.size());
-
-	for(int i = 0; i < vector1.size(); i++) {
-		output[i] = vector1[i] + vector2[i];
-	}
-
-	return output;
-}
-
-/* The dense layer class */
+template<std::size_t inputs_width, std::size_t inputs_height, std::size_t n_neurons>
 class DenseLayer {
-private:
-	vector<vector<double>> weights;
-	vector<double> biases;
-	vector<vector<double>> output;
-
 public:
-	/* Constructor */
-	DenseLayer(int n_inputs, int n_neurons) {
-		weights = vector<vector<double>>(n_inputs, vector<double>(n_neurons));
-		for(int i = 0; i < n_inputs; i++) {
-			for(int j = 0; j < n_neurons; j++) {
-				/* random weights initialization */
-				weights[i][j] = 0.1 * getRandomDouble(-1, 1);
-			}
-		}
-
-		// Initializing biases as a vector of zeros
-		biases = vector<double>(n_neurons, 0);
+	DenseLayer()
+	{		
+		randn(weights, 0.10);
+		zeros(biases);
+		std::cout << "Biases : \n";
+		pretty_print(biases);
 	}
 
-	/* forward pass */
-	void forward(vector<vector<double>> inputs) {
-		output = dotProduct(inputs, weights);
-		for(int i = 0; i < output.size(); i++) {
-			output[i] = add(output[i], biases);
-		}
+	template<class M>
+	auto forward(const M& inputs) ->
+		decltype(std::declval<M>().matrix, void()) 
+	{ 
+		outputs = dot(inputs, weights, biases);
 	}
+	Matrix<n_neurons, inputs_height> outputs;
 
-	void printOutput() {
-		for(int i = 0; i < output.size(); i++) {
-			for(int j = 0; j < output[0].size(); j++) {
-				cout << output[i][j] << " ";
-			}
-			cout << endl;
-		}
-	}
-
-	/* Getter for the output variable which is private */
-	vector<vector<double>> getOutput() {
-		return output;
-	}
+private:
+	Matrix<inputs_width, n_neurons> weights;
+	List<double, n_neurons> biases;
 };
 
-int main() {
-	/* Initializing two layers*/ 
-	DenseLayer l1(4, 5);
-	DenseLayer l2(5, 2);
+int main ()
+{
+	Matrix<4, 3> X {
+			std::array {1.0, 2.0, 3.0, 2.5},
+			std::array {2.0, 5.0, -1.0, 2.0},
+			std::array {-1.5, 2.7, 3.3, -0.8}};
 
-	/* Sample input */
-	vector<vector<double>> X = {
-		{1, 2, 3, 2.5},
-     	{2.0, 5.0, -1.0, 2.0},
-     	{-1.5, 2.7, 3.3, -0.8}
- 	};
+	DenseLayer<4,decltype(X)::height.get(), 5> denseLayer1;
+	DenseLayer<5,decltype(denseLayer1.outputs)::height.get(), 2> denseLayer2;
 
- 	cout << "First layer forward pass output:" << endl;
- 	l1.forward(X);
- 	l1.printOutput();
+	std::cout << "Inputs : \n";
+	pretty_print(X);
+	denseLayer1.forward(X);
+	std::cout << "Outputs : \n";
+	pretty_print(denseLayer1.outputs);
 
- 	cout << endl << "Second layer forward pass output:" << endl;
- 	l2.forward(l1.getOutput());
- 	l2.printOutput();
+
+	std::cout << "Inputs : \n";
+	pretty_print(denseLayer1.outputs);
+	denseLayer2.forward(denseLayer1.outputs);
+	std::cout << "Outputs : \n";
+	pretty_print(denseLayer2.outputs);
+
+	return 0;
 }
