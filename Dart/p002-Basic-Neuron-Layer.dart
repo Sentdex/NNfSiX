f7c9931113e49456cf2578cfa15c0dc038e6529b
Huzaifa Abid <huzaifaa926@gmail.com>
2020-05-07 20:06:26 +0500

    p002

---
+++ Dart/p002-Basic-Neuron-Layer.dart
@@ -0,0 +1,29 @@
+void main() 
+{
+  // Creating a basic neuron with 4 inputs, along with weights and biases that generates 3 outputs.
+  List<double> inputs = [1.0, 2.0, 3.0, 2.5];
+  List<double> weights1 = [0.2, 0.8, -0.5, 1.0];
+  List<double> weights2 = [0.5, -0.91, 0.26, -0.5];
+  List<double> weights3 = [-0.26, -0.27, 0.17, 0.87];
+
+  double bias1 = 2.0;
+  double bias2 = 3.0;
+  double bias3 = 0.5;
+  
+  List<double> output = [ inputs[0]*weights1[0] + 
+                          inputs[1]*weights1[1] + 
+                          inputs[2]*weights1[2] + 
+                          inputs[3]*weights1[3] + bias1,
+                         
+                          inputs[0]*weights2[0] + 
+                          inputs[1]*weights2[1] + 
+                          inputs[2]*weights2[2] + 
+                          inputs[3]*weights2[3] + bias2,
+                         
+                          inputs[0]*weights3[0] + 
+                          inputs[1]*weights3[1] + 
+                          inputs[2]*weights3[2] + 
+                          inputs[3]*weights3[3] + bias3
+                        ];
+  print(output);
+}
