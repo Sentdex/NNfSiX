e4d1e725540b3c5d38903c8ca0e42cf1d081b1c5
Cdimoy <50119145+Cdimoy@users.noreply.github.com>
2020-05-15 00:33:51 +0300

    Added part 5 in F# and nnfs module for spiral_data
    nnfs module contains the spiral_data generator and some utility for MathNet

---
+++ F#/p005-ReLU-Activation.fs
@@ -0,0 +1,19 @@
+﻿open MathNet.Numerics.LinearAlgebra
+open nnfs
+
+let X, y = spiral_data 100 3
+
+type LayerDense(nInputs : int, nNeurons : int) = 
+    let weights: Matrix<float> = 0.1 * DenseMatrix.random nInputs nNeurons NormalDistribution
+    let biases:  Vector<float> = DenseVector.zero nNeurons
+
+    member _.Forward inputs = 
+        Matrix.AddVectorToEachRow (inputs * weights) biases 
+
+
+type ActivationReLU() =
+    static member Forward = Matrix.map (max 0.0)
+
+let layer1 = LayerDense(2, 5)
+
+printfn "%A" (ActivationReLU.Forward << layer1.Forward <| X)
