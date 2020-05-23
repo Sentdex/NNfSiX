f35f4cc25899c3234229b369cae15b9bdc04516e
rupeshkmr <kumarrupesh955@gmail.com>
2020-05-23 10:16:31 +0530

    corrected spelling of bias in p004-Batches-Layers.c

--- C/p004-Batches-Layers.c
+++ C/p004-Batches-Layers.c
@@ -8,7 +8,7 @@
 
 #define RAND_HIGH_RANGE (0.10)
 #define RAND_MIN_RANGE (-0.10)
-#define INIT_BAISES (0.0)
+#define INIT_BIASES (0.0)
 
 #define NET_BATCH_SIZE 3
 #define NET_INPUT_LAYER_1_SIZE 4 // can be replaced with (sizeof(var)/sizeof(double))
@@ -18,46 +18,46 @@
 
 typedef struct{
     double *weights;
-    double *baise;
+    double *bias;
     double *output;
     int input_size;
     int output_size;
 }layer_dense_t;
 
-/**@brief Get the dot product of a neuron and add the bais.
+/**@brief Get the dot product of a neuron and add the bias.
  *
  * @param[in]   input   	Pointer to the first address of the inputs.
  * @param[in]   weights  	Pointer to the first address of the weights.
- * @param[in]   bais	 	Pointer to the value of the neurons bais.
+ * @param[in]   bias	 	Pointer to the value of the neurons bias.
  * @param[in]   input_size  number of neurons in the input layer.
  * @retval[out] output		the dot product of the neuron.
  */
 
-double dot_product(double *input,double *weights,double *bais,int input_size){
+double dot_product(double *input,double *weights,double *bias,int input_size){
     int i = 0;
     double output = 0.0;
     for(i = 0;i<input_size;i++){
         output += input[i]*weights[i];
     }
-    output += *bais;
+    output += *bias;
     return output;
 }
 
-/**@brief Get the dot products of each neuron and add the bais and store it in an output array.
+/**@brief Get the dot products of each neuron and add the bias and store it in an output array.
  *
  * @param[in]   	input   		Pointer to the first address of the inputs.
  * @param[in]   	weights  		Pointer to the first address of the weights.
- * @param[in]   	bais	 		Pointer to the first address of the neuron bases.
+ * @param[in]   	bias	 		Pointer to the first address of the neuron bases.
  * @param[in]   	input_size  	number of neurons in the input layer.
  * @param[in/out]   outputs  		Pointer to the first address of the outputs array.
  * @param[in]   	output_size  	number of neurons in the output layer.
  */
 
-void layer_output(double *input,double *weights,double *bais,int input_size,double *outputs,int output_size){
+void layer_output(double *input,double *weights,double *bias,int input_size,double *outputs,int output_size){
     int i = 0;
     int offset = 0;
     for(i = 0; i < output_size; i++){
-        outputs[i] = dot_product(input,weights + offset,&bais[i],input_size);
+        outputs[i] = dot_product(input,weights + offset,&bias[i],input_size);
         offset+=input_size;
     }
 }
@@ -71,7 +71,7 @@ double rand_range(double min, double max)
 }
 
 
-/**@brief Setup a layer with random weights and bais as well as allocating memory for the storage buffers.
+/**@brief Setup a layer with random weights and bias as well as allocating memory for the storage buffers.
  *
  * @param[in]   	layer   		Pointer to an empty layer with no values.
  * @param[in]   	intput_size  	size of the input layer.
@@ -93,9 +93,9 @@ void layer_init(layer_dense_t *layer,int intput_size,int output_size, int batch_
         printf("weights mem error\n");
         return;
     }
-    layer->baise   = malloc(sizeof(double) * output_size);
-    if(layer->baise == NULL){
-        printf("baise mem error\n");
+    layer->bias   = malloc(sizeof(double) * output_size);
+    if(layer->bias == NULL){
+        printf("bias mem error\n");
         return;
     }
     layer->output = malloc(sizeof(double) * output_size * batch_size);
@@ -107,7 +107,7 @@ void layer_init(layer_dense_t *layer,int intput_size,int output_size, int batch_
 
     int i = 0;
     for(i = 0; i < (output_size); i++){
-           layer->baise[i] = INIT_BAISES;
+           layer->bias[i] = INIT_BIASES;
     }
     for(i = 0; i < (intput_size*output_size); i++){
            layer->weights[i] = rand_range(RAND_MIN_RANGE,RAND_HIGH_RANGE);
@@ -119,10 +119,10 @@ void deloc_layer(layer_dense_t *layer){
     if(layer->weights != NULL){
         free(layer->weights);
     }
-    if(layer->baise != NULL){
-        free(layer->baise);
+    if(layer->bias != NULL){
+        free(layer->bias);
     }
-    if(layer->baise != NULL){
+    if(layer->bias != NULL){
         free(layer->output);
     }
 }
@@ -136,7 +136,7 @@ void deloc_layer(layer_dense_t *layer){
 void forward(layer_dense_t *previos_layer,layer_dense_t *next_layer, int batch_index){
     int offset = next_layer->output_size * batch_index;
     int input_offset = next_layer->input_size * batch_index;
-    layer_output((previos_layer->output + input_offset),next_layer->weights,next_layer->baise,next_layer->input_size,(next_layer->output + offset),next_layer->output_size);
+    layer_output((previos_layer->output + input_offset),next_layer->weights,next_layer->bias,next_layer->input_size,(next_layer->output + offset),next_layer->output_size);
 }
 
 
@@ -227,7 +227,7 @@ int main()
                                                                 {0.5, -0.91, 0.26, -0.5},
                                                                 {-0.26, -0.27, 0.17, 0.87},
                                                                 };
-    double bais[NET_HIDDEN_LAYER_2_SIZE] = {2.0,3.0,0.5};
+    double bias[NET_HIDDEN_LAYER_2_SIZE] = {2.0,3.0,0.5};
 
 
     double layer1_output[NET_BATCH_SIZE][NET_HIDDEN_LAYER_2_SIZE];
@@ -237,7 +237,7 @@ int main()
                                                                 {-0.5, 0.12, -0.33},
                                                                 {-0.44, 0.73, -0.13},
                                                                 };
-    double bais2[NET_OUTPUT_LAYER_SIZE] = {-1.0,2.0,-0.5};
+    double bias2[NET_OUTPUT_LAYER_SIZE] = {-1.0,2.0,-0.5};
 
     double layer2_output[NET_BATCH_SIZE][NET_OUTPUT_LAYER_SIZE];
 
@@ -247,14 +247,14 @@ int main()
     }
 
     layer1.weights = weights;
-    layer1.baise = bais;
+    layer1.bias = bias;
     layer1.input_size = NET_INPUT_LAYER_1_SIZE;
     layer1.output_size = NET_HIDDEN_LAYER_2_SIZE;
     layer1.output = layer1_output;
 
 
     layer2.weights = weights2;
-    layer2.baise = bais2;
+    layer2.bias = bias2;
     layer2.input_size = NET_HIDDEN_LAYER_2_SIZE;
     layer2.output_size = NET_OUTPUT_LAYER_SIZE;
     layer2.output = layer2_output;
