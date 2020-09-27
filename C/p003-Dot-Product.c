6cadc9a2cfcdbbf50272de940a062f84f495026a
William Brumble <45638531+MinimotoMusashi@users.noreply.github.com>
2020-09-27 07:17:51 -0500

    Update p003-Dot-Product.c
    Creating this pull request, to simplify the code, and make it better match the code that's in the book and video. Also adding dash in file name/title.

--- C/p003-Dot-Product.c
+++ C/p003-Dot-Product.c
@@ -10,13 +10,13 @@ int main() {
     float weights[3][4] = {{0.2f, 0.8f, -0.5f, 1.0f},{0.5f, -0.91f, 0.26f, -0.5f},{-0.26f, -0.27f, 0.17f, 0.87f}};
     float biases[3] = {2.0f, 3.0f, 0.5f};
     float neuron_output, layer_outputs[3];
-    int inner_loop, outer_loop;
-    for(outer_loop = 0; outer_loop < 3; outer_loop++){
+    int n_inputs, n_neurons;
+    for(n_neurons = 0; n_neurons < 3; n_neurons++){
         neuron_output = 0;
-        for(inner_loop = 0; inner_loop < 4; inner_loop++)
-            neuron_output += weights[outer_loop][inner_loop] * inputs[inner_loop];
-        neuron_output += biases[outer_loop];
-        layer_outputs[outer_loop] = neuron_output;
+        for(n_inputs = 0; n_inputs < 4; n_inputs++)
+            neuron_output += weights[n_neurons][n_inputs] * inputs[n_inputs];
+        neuron_output += biases[n_neurons];
+        layer_outputs[n_neurons] = neuron_output;
     }
     printf("[%f, %f, %f]\n", layer_outputs[0], layer_outputs[1], layer_outputs[2]);
     return 0;
