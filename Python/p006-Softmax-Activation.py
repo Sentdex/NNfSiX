ddab7b629d13344624004644ce19a229f926efef
Pritish Mishra <55872694+pritishmishra703@users.noreply.github.com>
2020-12-21 18:38:45 +0530

    Create p006-Softmax-Activation.py

---
+++ Python/p006-Softmax-Activation.py
@@ -0,0 +1,40 @@
+import numpy as np 
+import nnfs
+from nnfs.datasets import spiral_data
+
+nnfs.init()
+
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, keepdims=True, axis=1))
+        probabilities = exp_values/np.sum(exp_values, keepdims=True, axis=1)
+        self.output = probabilities
+
+
+X, y = spiral_data(100, 3)   
+
+dense1 = Layer_Dense(2,3)
+activation1 = Activation_ReLU()
+
+dense2 = Layer_Dense(3, 3)
+activation2 = Activation_Softmax()
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])
