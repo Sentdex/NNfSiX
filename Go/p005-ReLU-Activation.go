a2f7ee5286cfc438ff1c1dc1a00eb5aac9c540eb
mbdeveci <mhmtbrkdvc@gmail.com>
2020-12-24 22:17:52 +0300

    Merge files in a single file

--- Go/p005-ReLU-Activation.go
+++ Go/p005-ReLU-Activation.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 	"p005/nnfs"
 
@@ -71,9 +72,9 @@ func (a *ActivationRelu) Forward(input *mat.Dense) {
 
 func main() {
 	rand.Seed(0)
-	X, _ := nnfs.NewSpiralData(100, 3)
+	X, y := nnfs.NewSpiralData(100, 3)
 
-	// plotSpiral(X, y)
+	plotSpiral(X, y)
 
 	layer1 := NewLayerDense(2, 5)
 	activation1 := NewActivationRelu()
@@ -118,4 +119,34 @@ func plotSpiral(X *mat.Dense, y *mat.Dense) {
 	if err := p.Save(4*vg.Inch, 4*vg.Inch, "points.png"); err != nil {
 		panic(err)
 	}
+}
+
+// NewSpiralData generates spiral data. see: https://cs231n.github.io/neural-networks-case-study/
+func NewSpiralData(numberOfPoints, numberOfClasses int) (*mat.Dense, *mat.Dense) {
+	X := mat.NewDense(numberOfPoints*numberOfClasses, 2, nil)
+	y := mat.NewDense(numberOfPoints*numberOfClasses, 1, nil)
+
+	for c := 0; c < numberOfClasses; c++ {
+		radius := linspace(0, 1, numberOfPoints)
+		t := linspace(float64(c*4), float64((c+1)*4), numberOfPoints)
+		for i := range t {
+			t[i] += 0.2 * rand.NormFloat64()
+		}
+		for i := 0; i < numberOfPoints; i++ {
+			X.Set(c*numberOfPoints+i, 0, radius[i]*math.Sin(t[i]*2.5))
+			X.Set(c*numberOfPoints+i, 1, radius[i]*math.Cos(t[i]*2.5))
+			y.Set(c*numberOfPoints+i, 0, float64(c))
+		}
+	}
+
+	return X, y
+}
+
+func linspace(start, end float64, num int) []float64 {
+	result := make([]float64, num)
+	step := (end - start) / float64(num-1)
+	for i := range result {
+		result[i] = start + float64(i)*step
+	}
+	return result
 }
