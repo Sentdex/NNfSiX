e681b57c4d8e6496661758f6defcaebadfbc15ce
George Fotopoulos <xorz57@protonmail.com>
2021-01-05 16:16:12 -0800

    Use std::array
    Its much safer than C arrays!

--- C++/p001-Basic-Neuron-3-inputs.cpp
+++ C++/p001-Basic-Neuron-3-inputs.cpp
@@ -4,11 +4,12 @@
  * Associated YT NNFS tutorial: https://www.youtube.com/watch?v=Wo5dMEP_BbI
  */
 
+#include <array>
 #include <iostream>
 
 int main() {
-    double inputs[] = {1.0, 2.0, 3.0};
-    double weights[] = {3.1, 2.1, 8.7};
+    std::array<double, 3> inputs = {1.0, 2.0, 3.0};
+    std::array<double, 3> weights = {3.1, 2.1, 8.7};
     double bias = 3.0;
 
     double output = inputs[0] * weights[0] +
