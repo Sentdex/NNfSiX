1280d75111c79fa74e9d58b9404bf473c4b468b8
George Fotopoulos <xorz57@protonmail.com>
2021-01-12 18:48:30 -0800

    Update p002-Basic-Neuron-Layer.cpp

--- C++/p002-Basic-Neuron-Layer.cpp
+++ C++/p002-Basic-Neuron-Layer.cpp
@@ -18,7 +18,7 @@ int main() {
 	double bias2 = 3.0;
 	double bias3 = 0.5;
 
-	double output[] = {
+	std::array<double, 3> outputs = {
 			  inputs[0]*weights1[0] + inputs[1]*weights1[1] + inputs[2]*weights1[2] + inputs[3]*weights1[3] + bias1,
 			  inputs[0]*weights2[0] + inputs[1]*weights2[1] + inputs[2]*weights2[2] + inputs[3]*weights2[3] + bias2,
 			  inputs[0]*weights3[0] + inputs[1]*weights3[1] + inputs[2]*weights3[2] + inputs[3]*weights3[3] + bias3
