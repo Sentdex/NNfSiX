4d320bbb4dede8b48b9205a7a8b53fb9eaffa8cf
George Fotopoulos <xorz57@protonmail.com>
2021-01-12 18:41:59 -0800

    Update p003-Dot-Product.cpp

--- C++/p003-Dot-Product.cpp
+++ C++/p003-Dot-Product.cpp
@@ -4,50 +4,41 @@
  * Associated tutorial https://www.youtube.com/watch?v=tMrbN67U9d4
  */
 
+#include <array>
 #include <iostream>
-#include <vector>
-using std::vector;
-
-
-vector<double> dotProduct(vector<vector<double>> weights, vector<double> inputs) {
-	vector<double> outputs(weights.size());
-
-	for(int i = 0; i < weights.size(); i++) {
-		double output = 0;
-		for(int j = 0; j < inputs.size(); j++) {
-			output += weights[i][j] * inputs[j];
-		}
-		outputs[i] = output;
-	}
-
-	return outputs;
-}
-
-vector<double> add(vector<double> vector1, vector<double> vector2) {
-	vector<double> output(vector1.size());
-
-	for(int i = 0; i < vector1.size(); i++) {
-		output[i] = vector1[i] + vector2[i];
-	}
-
-	return output;
-}
-
 
 int main() {
+	std::array<double, 4> inputs = {
+		+1.00,
+		+2.00,
+		+3.00,
+		+2.50
+	};
 
-	vector<double> inputs = {1.0, 2.0, 3.0, 2.5};
+	std::array<double, 3 * 4> weights = {
+		+0.20, +0.80, -0.50, +1.00,
+		+0.50, -0.91, +0.26, -0.50,
+		-0.26, -0.27, +0.17, +0.87
+	};
 
-	vector<vector<double>> weights = {{0.2, 0.8, -0.5, 1.0},
-									  {0.5, -0.91, 0.26, -0.5},
-									  {-0.26, -0.27, 0.17, 0.87}};
+	std::array<double, 3> biases = {
+		+2.00,
+		+3.00,
+		+0.50
+	};
 
-    vector<double> biases = {2.0, 3.0, 0.5};
+	std::array<double, 3> outputs;
 
-	vector<double> output = add(dotProduct(weights, inputs), biases);
+	// output = weights * inputs + biases
+	for(std::size_t row = 0; row < 3; ++row) {
+		double sum = 0;
+		for(std::size_t col = 0; col < 4; ++col) {
+			sum += weights[row * 4 + col] * inputs[col];
+		}
+		outputs[row] = sum + biases[row];
+	}
 
-	std::cout << "[";
-	std::cout << output[0] << ", ";
-	std::cout << output[1] << ", ";
-	std::cout << output[2] << "]" << std::endl;
+	std::cout << "[" << outputs[0] << ", " << outputs[1] << ", " << outputs[2] << "]" << std::endl;
+
+	return 0;
 }
