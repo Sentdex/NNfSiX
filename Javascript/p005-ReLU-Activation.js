fdb33c8bbb65ae9bdc03b51bd5aa984b9de84ecd
Daniel Kukieła <daniel@kukiela.pl>
2021-01-16 04:08:24 +0100

    Added spiral data into p005

--- Javascript/p005-ReLU-Activation.js
+++ Javascript/p005-ReLU-Activation.js
@@ -5,11 +5,42 @@ Associated YT tutorial: https://www.youtu.be/gmjzbpSVY1A
 
 const math = require("mathjs");
 
-// There is no nnfs library for JS, so I am just using the previous data.
-const X = math.matrix([
-	[1.0, 2.0, 3.0, 2.5],
-	[2.0, 5.0, -1.0, 2.0],
-	[-1.5, 2.7, 3.3, -0.8]]);
+// Moved this code from spiral-data.js written by @vancegillies
+// Updated by @daniel-kukiela
+function spiral_data(points, classes) {
+  // Using MathJs functions to make matrices with zeros but converting to arrays for simplicity
+  const X = math.zeros(points * classes, 2).toArray();
+  const y = math.zeros(points * classes, "dense").toArray();
+  let ix = 0;
+  for (let class_number = 0; class_number < classes; class_number++) {
+    let r = 0;
+    let t = class_number * 4;
+
+    while (r <= 1 && t <= (class_number + 1) * 4) {
+      // adding some randomness to t
+      const random_t = t + math.random(points) * 0.008;
+      // Was `* 0.2` but reduced so you can somewhat see the arms of spiral in visualization
+      // Fell free to change it back
+
+      // converting from polar to cartesian coordinates
+      X[ix][0] = r * math.sin(random_t * 2.5);
+      X[ix][1] = r * math.cos(random_t * 2.5);
+      y[ix] = class_number;
+
+      // the below two statements achieve linspace-like functionality
+      r += 1.0 / (points - 1);
+      t += 4.0 / (points - 1);
+
+      ix++; // increment index
+    }
+  }
+  // Returning as MathJs matrices, could be arrays, doesnt really matter
+  return [math.matrix(X), math.matrix(y)];
+}
+
+// Updated to use the spiral_data function
+// Updated by @daniel-kukiela
+let [X, y] = spiral_data(100, 3);
 
 class Layer_Dense {
 	constructor (n_inputs, n_neurons) {
