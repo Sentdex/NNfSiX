8c7e3dfedb58fd1aaf0453b6ec7c051d6861e0a9
Cdimoy <50119145+Cdimoy@users.noreply.github.com>
2021-02-10 00:56:22 +0300

    Create p007-Categorical-Cross-Entropy-Loss.fs

---
+++ F#/p007-Categorical-Cross-Entropy-Loss.fs
@@ -0,0 +1,13 @@
+open type System.Math
+
+let softmax_output = [0.7; 0.1; 0.2]
+let target_output  = [1; 0; 0]
+
+let loss = -(Log(softmax_output.[0]) * float target_output.[0] + 
+             Log(softmax_output.[1]) * float target_output.[1] +
+             Log(softmax_output.[2]) * float target_output.[2])
+           
+printfn "%f" loss
+
+printfn "%f" (-Log 0.7)
+printfn "%f" (-Log 0.5)
