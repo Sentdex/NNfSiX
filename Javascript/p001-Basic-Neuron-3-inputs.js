b69b8503e5e1bd415cdeaa1a088d7395c284ee16
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2023-01-15 14:20:09 +1100

    Update p001-Basic-Neuron-3-inputs.js

--- Javascript/p001-Basic-Neuron-3-inputs.js
+++ Javascript/p001-Basic-Neuron-3-inputs.js
@@ -2,12 +2,13 @@
  * Create basic neuron with 3 inputs in Javascript.
  */
 
-let inputs = [1.2, 5.1, 2.1];
-let weights = [3.1, 2.1, 8.7];
-let bias = 3.0
-let output = inputs[0] * weights[0] +
-    inputs[1] * weights[1] +
-    inputs[2] * weights[2] +
-    bias;
+const inputs  = [1.2, 5.1, 2.1];
+const weights = [3.1, 2.1, 8.7];
+const bias    = 3.0;
 
-console.log(`Predict : ${output}`)
+const output = inputs[0] * weights[0] +
+               inputs[1] * weights[1] +
+               inputs[2] * weights[2] +
+               bias;
+
+console.log(output);
