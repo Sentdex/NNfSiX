adbb08d7f48a6f5f8ab266220f2d687228dd9b44
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2023-01-15 17:31:30 +1100

    Update p003-Dot-Product.js

--- Javascript/p003-Dot-Product.js
+++ Javascript/p003-Dot-Product.js
@@ -1,18 +1,59 @@
+const inputs  = [1, 2, 3, 2.5];
+
+const weights = [
+    [ 0.2,   0.8,  -0.5,   1   ],
+    [ 0.5,  -0.91,  0.26, -0.5 ],
+    [-0.26, -0.27,  0.17,  0.87]
+];
+
+const biases  = [2, 3, 0.5];
+
 /*
-Creates a simple layer of neurons using dot product.
-Associated YT NNFS tutorial: https://www.youtube.com/watch?v=tMrbN67U9d4
+const layer_outputs = []; // Output of current layer
+for (let i = 0; i < weights.length; i++) {
+    // weights[i] = neuron_weights
+    // biases[i]  = neuron_bias
+    let neuron_output = 0; // Output of given neuron
+    for (let j = 0; j < inputs.length; j++) {
+        // inputs[j]     = n_input
+        // weights[i][j] = weight
+        neuron_output += inputs[j]*weights[i][j];
+    }
+    neuron_output += biases[i];
+    layer_outputs.push(neuron_output);
+}
+
+console.log(layer_outputs);
 */
 
-const math = require("mathjs");
+// assumes a is 2D, b is 1D and all nested-arrays in a are same length as b which suffices for this use case
+function dot(a, b) {
+    const res = [];
+    for (let i = 0; i < a.length; i++) {
+        let dot = 0;
+        for (let j = 0; j < b.length; j++) {
+            dot += a[i][j] * b[j];
+        }
+        res.push(dot);
+    }
+    return res;
+}
 
-const inputs = [1.0, 2.0, 3.0, 2.5];
-const weights = math.matrix([
-  [0.2, 0.8, -0.5, 1.0],
-  [0.5, -0.91, 0.26, -0.5],
-  [-0.26, -0.27, 0.17, 0.87],
-]);
+// assumes you are adding b on to a, where b can be a number or an array
+function add(a, b) {
+    if (Array.isArray(a) && typeof b === "number") {
+        const res = [];
+        for (let i = 0; i < a.length; i++) {
+            res.append(a[i] + b);
+        }
+        return res;
+    }
+    const res = [];
+    for (let i = 0; i < a.length; i++) {
+        res.push(a[i] + b[i]);
+    }
+    return res;
+}
 
-const biases = [2.0, 3.0, 0.5];
-
-let output = math.add(math.multiply(weights, inputs), biases);
+const output = add(dot(weights, inputs), biases);
 console.log(output);
