95ff548564f241acbde84814a2183866b1b68336
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2023-01-15 14:29:01 +1100

    Update p001-Basic-Neuron-3-inputs.js

--- Javascript/p001-Basic-Neuron-3-inputs.js
+++ Javascript/p001-Basic-Neuron-3-inputs.js
@@ -4,7 +4,7 @@
 
 const inputs  = [1.2, 5.1, 2.1];
 const weights = [3.1, 2.1, 8.7];
-const bias    = 3.0;
+const bias    = 3;
 
 const output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias;
 
